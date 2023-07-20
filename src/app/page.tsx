7c42ace86ba04ebe79cffa2aa3c12868ebb15d56
ibrokhim <mr017programmer@gmail.com>
2023-07-20 17:24:11 +0500

    pravki

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import { Container, UploadWrapper } from "@/components/Shared";
 
 export default function Home() {
   return (
-    <div className={"aaa"}>
-      <Container>
-        <UploadWrapper />
-      </Container>
-    </div>
+    <Container>
+      <UploadWrapper />
+    </Container>
   );
 }
