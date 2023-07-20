d8ecc3a8800c373a4004b38a58ace72951a0b82a
ibrokhim <mr017programmer@gmail.com>
2023-07-20 09:41:18 +0500

    Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import classes from "./Header.module.scss";
+import { Container } from "../Shared";
 
 const Header = () => {
   return (
     <header className={classes.header}>
-      <h1>Header</h1>
+      <Container>
+        <div className={classes.inner}>
+          <div className={classes.logo}>
+            <span>Logo</span>
+          </div>
+
+          <div className={classes.right}>
+            <div>menu</div>
+          </div>
+        </div>
+      </Container>
     </header>
   );
 };
